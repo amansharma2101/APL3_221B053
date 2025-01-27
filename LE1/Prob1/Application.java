0337502a40c7fe329022a700341838a430da05e2
221B053 <221B053@juet.co.in>
2025-01-27 14:36:20 +0530

    Lab 1 done

---
+++ LE1/Prob1/Application.java
@@ -0,0 +1,10 @@
+public class Application {
+    public static void main(String[] args) {
+        
+        Mother m = new Mother();
+        m.show(); 
+       
+        Child ch = new Child();
+        ch.show();
+    }
+}
---
+++ LE1/Prob1/Mother.java
@@ -0,0 +1,7 @@
+class Mother {
+    
+    void show() {
+        System.out.println("This is the show method of Mother class");
+    }
+}
+
