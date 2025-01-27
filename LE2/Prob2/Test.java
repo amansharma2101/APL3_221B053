e491327b40eed0703a6b090561718f6bf7bd2b05
221B053 <221B053@juet.co.in>
2025-01-27 15:43:55 +0530

    Lab 2 done

---
+++ LE2/Prob2/Test.java
@@ -0,0 +1,7 @@
+public class Test {
+    public static void main(String[] args) {
+        Voice voice = new Voice();
+        voice.prepareVoice(); // Prepare the voices
+        //voice.hear(); // Hear the voices
+    }
+}
